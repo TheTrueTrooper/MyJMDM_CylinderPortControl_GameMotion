c32c2b1fac81215c44447f3825cda4b37a1d65a2
Angelo <Soviet_Trooper@live.ca>
2019-07-10 17:58:09 -0600

    cvx

--- JMDM_Basic_Ride_Control/JMDM_BasicRide.cs
+++ JMDM_Basic_Ride_Control/JMDM_BasicRide.cs
@@ -14,7 +14,7 @@ namespace JMDM_Basic_Ride_Control
         public static void JMDM_BasicRide_Run(string Port, StreamReader CommandData, Action ExtraAction = null)
         {
             List<CommandSetLine> LineCommands = CommandSetLine.LoadLineCommandsFromTextStream(CommandData);
-            JMDM_CylinderPortControl CylinderControl = new JMDM_CylinderPortControl(Port, LineCommands[0].CylinderHeights.Count());
+            JMDM_CylinderPortControlUpdated CylinderControl = new JMDM_CylinderPortControlUpdated(Port, LineCommands[0].CylinderHeights.Count());
 
             CylinderControl.Open_Port();
 
